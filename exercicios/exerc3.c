5a2bb727c6d3228da90c24b2977d3610b9308896
gustevito <dev.gustavo.flores@gmail.com>
2025-08-10 18:28:16 -0300

    exercixios

---
+++ exercicios/exerc3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Digite dois nºs:\n");
+    int n1, n2;
+    scanf("%d %d",&n1,&n2);
+    printf("Números digitados: %d %d" + n1,n2);
+    return 0;
+}
