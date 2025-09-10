6078049e8a180f4aa92da021ccb9715d9fa9e88d
gustevito <dev.gustavo.flores@gmail.com>
2025-09-10 13:28:49 -0300

    questoes da prova incompletas

---
+++ prova1/1/src/subvet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void subVets(int* vetOrig, int* vetInis, int* vetTams, int* total);
+
+int main(int argc, char const *argv[])
+{
+    int vetorOriginal[] = { 11, 2, 0, 1, 6, 1, 0, 0, 1, 1, 1 };
+    int vetInicios[MAX], vetTamanhos[MAX];
+    int totalSubVets;
+    return 0;
+}
+
+void subVets(int* vetOrig, int* vetInis, int* vetTams, int* total){
+    int tam = vetOrig[0];
+    int pos = 1;
+
+    while (pos < tam)
+    {
+        int qtdElemSub = vetOrig[pos];
+        int posIniSub = pos + 1;
+        printf("pos: %d, %d elems.\n", posIniSub, qtdElemSub);
+        vetInis[qtdSubVets]
+    }
+    
+
+
+    
+}
+
