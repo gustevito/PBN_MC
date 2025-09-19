9bba336d32154c30c3794094ac894c08195678bf
gustevito <dev.gustavo.flores@gmail.com>
2025-09-19 09:15:02 -0300

    malloc

---
+++ exercicios/structs/exemplo/src/struct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct {
+    float p1;
+    float p2;
+    float trab;
+    int faltas;
+} Aluno;
+
+void exibeAluno();
+void exibeTurma();
+
+int main(int argc, char const *argv[])
+{
+    Aluno aluno1, aluno2;
+    return 0;
+}
